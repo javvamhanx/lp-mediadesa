8876013fe89c4de0a92d81c72356766567eb44c7
rahmat Hidayat <rahmatrit@gmail.com>
2020-07-25 10:41:30 +0700

    commit awal

---
+++ script.js
@@ -0,0 +1,14 @@
+const abuRokok = document.getElementById('aburokok');
+abuRokok.onclick = function () {
+    document.body.setAttribute('class', 'aburokok');
+}
+
+const hijaumuda = document.getElementById('hijaumuda');
+hijaumuda.onclick = function () {
+    document.body.setAttribute('class', 'hijaumuda');
+}
+
+const merah = document.getElementById('merah');
+merah.onclick = function () {
+    document.body.setAttribute('class', 'merah');
+}
